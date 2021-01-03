2704ed4237575d2b92399c3b438a0f9ce18a0a42
wqy <1297203012@qq.com>
2021-01-03 16:55:26 +0800

    完成了首页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,23 @@
+//每次调用$.get()或$.post()或$.ajax()的时候，会先调用 ajaxPrefilter 这个函数，在这个函数中可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function (options) {
+    //在发起真正的Ajax请求之前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url;
+
+    //统一为有权限的接口设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    //全局统一挂载 complete 回调函数
+    options.complete = function (res) {
+        //可以使用res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //强制清空token
+            localStorage.removeItem('token');
+            //强制跳转登录页面
+            location.href = "../../login.html";
+        }
+    }
 })
